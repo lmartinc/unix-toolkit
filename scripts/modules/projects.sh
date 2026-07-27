1995fe6cb8e48b8b66d6fbaed6e66dbaa51566e9
Lance Martincich <lmartincich@gmail.com>
2026-07-27 08:38:30 +0200

    projects.sh path issue

--- scripts/modules/projects.sh
+++ scripts/modules/projects.sh
@@ -4,7 +4,10 @@
 # Create projects directory
 ###############################################################################
 
-source "${HOME}/.unix-toolkit/lib/common.sh"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+TOOLKIT_ROOT="$(cd "${SCRIPT_DIR}/../.." && pwd)"
+
+source "${TOOLKIT_ROOT}/lib/common.sh" 
 
 create_directory "${PROJECTS_HOME}"
 
