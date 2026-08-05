67cf382b6e60f251060a9580f63f9062a1b5028f
Lance Martincich <lmartincich@gmail.com>
2026-08-05 07:12:40 +0200

    Moved check_collection and check_project from ans-check to ansible.sh

--- lib/ansible.sh
+++ lib/ansible.sh
@@ -47,3 +47,61 @@ get_required_collections()
         }
     ' requirements.yml
 }
+
+###############################################################################
+# Check one collection
+###############################################################################
+
+check_collection()
+{
+    local collection="$1"
+    local installed
+    local version
+
+    installed="$(
+        grep "^${collection}[[:space:]]" <<< "${INSTALLED_COLLECTIONS}"
+    )"
+
+    if [[ -n "${installed}" ]]
+    then
+        version="$(awk '{print $2}' <<< "${installed}")"
+
+        log_status true "${collection}" "${version}"
+
+        return 0
+    fi
+
+    log_status false "${collection}"
+
+    return 1
+}
+
+###############################################################################
+# Check project structure
+###############################################################################
+
+check_project()
+{
+    section "Project Structure"
+
+    ((CHECKS+=1))
+    check_file ansible.cfg || ((FAILURES+=1))
+
+    ((CHECKS+=1))
+    check_file requirements.yml || ((FAILURES+=1))
+
+    ((CHECKS+=1))
+    check_directory inventory || ((FAILURES+=1))
+
+    ((CHECKS+=1))
+    check_directory playbooks || ((FAILURES+=1))
+
+    ((CHECKS+=1))
+    check_directory roles || ((FAILURES+=1))
+
+    ((CHECKS+=1))
+    check_directory group_vars || ((FAILURES+=1))
+
+    ((CHECKS+=1))
+    check_directory host_vars || ((FAILURES+=1))
+}
